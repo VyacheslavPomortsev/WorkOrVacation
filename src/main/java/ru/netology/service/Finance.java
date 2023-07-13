f454ef3782e80cc9a250205b71e70cec9a6d2f8a
Вячеслав Поморцев <xaoasakura1991@gmail.com>
2023-07-13 20:27:15 +0300

    change 2

--- src/main/java/ru/netology/service/Finance.java
+++ src/main/java/ru/netology/service/Finance.java
@@ -9,7 +9,7 @@ public class Finance {
         for (int month = 0; month < 12; month++) {
             if (money >= threshold) { // можем ли отдыхать?
                 count++; // увеличиваем счётчик месяцев отдыха
-                money = money - expenses - (money * 57 / 100);
+                money = (money - expenses) / 3;
             } else {
                 money = money + income - expenses;
             }
